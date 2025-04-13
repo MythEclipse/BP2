b06f998e62ac6222b3d299c9c8eaaeb0bbb71c17
MythEclipse <asepharyanalm001@gmail.com>
2025-04-13 15:01:30 +0700

    Refactor code structure for improved readability and maintainability

---
+++ src/main/java/com/mytheclipse/bp2m1/BP1M1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mytheclipse.bp2m1;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class BP1M1 {
+
+    public static void main(String[] args) {
+        FMenus a = new FMenus();
+        a.setVisible(true);
+    }
+}

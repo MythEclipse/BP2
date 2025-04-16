55bc5592c083d6ff60ddb6742d11286bbff80ca5
MythEclipse <asepharyanalm001@gmail.com>
2025-04-16 15:40:22 +0700

    Add Login class and form implementation for user authentication

---
+++ src/main/java/com/mytheclipse/bp2m2/BP2M2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mytheclipse.bp2m1;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class BP2M1 {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}

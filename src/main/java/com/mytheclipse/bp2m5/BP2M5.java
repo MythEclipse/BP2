ae9659ef034328a0c0f9c54db432f201b964ece5
MythEclipse <asepharyanalm001@gmail.com>
2025-05-14 15:37:01 +0700

    Add FUser and Koneksi classes for user management and database connection; update pom.xml for MySQL dependency

--- src/main/java/com/mytheclipse/bp2m5/BP2M5.java
+++ src/main/java/com/mytheclipse/bp2m5/BP2M5.java
@@ -2,16 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mytheclipse.bp2m3;
+package com.mytheclipse.bp2m5;
+
 
 
 /**
  *
  * @author PREDATOR
  */
-public class BP2M3 {
+public class BP2M5 {
 
     public static void main(String[] args) {
-       new Register().setVisible(true);
+     
     }
 }
